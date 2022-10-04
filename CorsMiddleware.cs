bcbe1296e8d150dfdd24d1c6563653cd930a792c
unknown <vaughnjbauer@gmail.com>
2022-10-04 11:04:23 -0500

    Allow all methods for options requests

--- CorsMiddleware.cs
+++ CorsMiddleware.cs
@@ -15,7 +15,14 @@ public class CorsMiddleware
         context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
         context.Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
+        
+        if(context.Request.Method == "OPTIONS")
+        {
+            context.Response.Headers.Add("Allow", "GET, POST, PUT, DELETE, OPTIONS");
+            context.Response.StatusCode = 200;
+            return;
+        }
+        
         await _next(context);
     }
 }
